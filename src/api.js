9433e3ee56330cf87651488975d96bc239171f10
adithyan s kumar <adithyans1438@gmail.com>
2024-11-26 15:22:31 +0530

    backend url change

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
-export const STREAM_URL = "https://cineflixserver.onrender.com/api/movies/stream-video"
+export const STREAM_URL = "http://3.89.223.135:3000/api/movies/stream-video"
 
-export const REACT_APP_API_URL = "https://cineflixserver.onrender.com/api/"
+export const REACT_APP_API_URL = "http://3.89.223.135:3000/api/"
